30fc3260512fd27457f6bcaca3169278c0b54c96
pedroncios <pedroyamada@gmail.com>
2024-10-31 16:49:54 -0300

    Implementação componente `footer`

--- app-imobiliaria/src/components/Footer.jsx
+++ app-imobiliaria/src/components/Footer.jsx
@@ -1,5 +1,27 @@
+import { Home, Mail, Phone } from "lucide-react";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-emerald-500 text-white px-4 py-8">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div className="flex items-center space-x-2 mb-4 md:mb-0">
+          <Home className="h-6 w-6" />
+          <span className="text-xl font-bold">ReactImóveis</span>
+        </div>
+
+        <div className="flex space-x-4">
+          <a href="#" className="flex items-center space-x-2 hover:underline">
+            <Mail className="w-4 h-4" />
+            <span>contato@reactimoveis.com</span>
+          </a>
+          <a href="#" className="flex items-center space-x-2 hover:underline">
+            <Phone className="w-4 h-4" />
+            <span>+55 (11) 1234-5678</span>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
